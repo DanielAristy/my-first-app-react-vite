8078e8fa3147e135bbea62b385742bd547726acf
daniel.aristizabal <daniel.aristizabal@sofka.co.co>
2024-05-06 18:09:47 -0500

    Manejo de PropTypes en React, se debe agregar la dependencia en Vite ya que no lo incluye inicialmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
 
-const App = () => {
+import PropTypes from 'prop-types';
+
+
+const newMensaje = 'Daniel Aristizabal';
+const objeto = {
+  titulo: 'Hola mundo',
+  mensaje: 'que mas pues '
+}
+
+const App = ({title}) => {
   return (
-    <h1>Hola mundo</h1>
+    <>
+      <h1>{title} {newMensaje}</h1>
+      <code>{JSON.stringify(objeto)}</code>
+      <p>Esto es un parrafo</p>
+    </>
   )
 }
 
 export default App
+
+App.propTypes = {
+  title: PropTypes.string.isRequired
+};
