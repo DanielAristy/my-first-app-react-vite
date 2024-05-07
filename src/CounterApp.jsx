da62993f946680a5d290b48101bcc822b9ec7469
daniel.aristizabal <daniel.aristizabal@sofka.co.co>
2024-05-06 21:49:36 -0500

    Uso del hook useState

---
+++ src/CounterApp.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const CounterApp = ({value}) => {
+    const [ count, setCount ] = useState(value);
+
+    const handleAdd = () => setCount( count + 1 );
+
+
+    const handleSubstract = () => setCount( count - 1 );
+
+    const handleReset = () => setCount(value);
+    
+  return (
+    <>
+        <h1>CounterApp {count}</h1>
+        <button type="button" onClick={ handleAdd }>+</button>
+        <button type="button" onClick={ handleSubstract }>-</button>
+        <button type="button" onClick={ handleReset }>Reset</button>
+    </>
+  )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+  };
+
+export default CounterApp;
