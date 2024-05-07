c947b732acb957125691971e9e3a8cafd4d3e4f6
daniel.aristizabal <daniel.aristizabal@sofka.co.co>
2024-05-07 16:37:51 -0500

    Se realiza prueba de 05-funciones de los metodos getUser y getUsuarioActivo

---
+++ src/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,33 @@
+import { getUser, getUsuarioActivo } from '../../base-pruebas/05-funciones'
+
+
+describe('Pruebas en 05-funciones', () => { 
+    test('getUser debe retornar un objeto', () => { 
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+
+        //expect( user ).toStrictEqual(testUser);
+        expect( user ).toEqual(testUser);
+     })
+
+     test('getUsuarioActivo debe retornar un objeto', () => { 
+        const name = 'Daniel'
+        //const testObject = {
+        //    uid: 'ABC567',
+        //    username: name
+        //}
+
+        const activeUser = getUsuarioActivo(name)
+
+        //expect( activeUser ).toEqual( testObject )
+        expect( activeUser ).toStrictEqual({
+            uid: 'ABC567',
+            username: name
+        })
+
+      })
+ })
