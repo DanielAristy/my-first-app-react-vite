9058f6c2d78ca957f1f0cc619781a0a2895c6297
daniel.aristizabal <daniel.aristizabal@sofka.co.co>
2024-05-07 18:16:21 -0500

    Pruebas de 1-async-await en el metodo de getImagen. Se realiza configuracion de Jest.config para el manejo de fetch

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -3,30 +3,23 @@
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try {
 
         const apiKey = '3kuQe8dIUCTsX9DAMpNsikPiFLtHR4t6';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
+        const res = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const { data } = await res.json(); 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
+        return url;
 
     } catch (error) {
         // manejo del error
         console.error(error)
+        return 'No se encontro la imagen'
     }
-    
-    
-    
 }
-
- getImagen();
 
 
 
---
+++ src/tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,12 @@
+import { getImagen } from '../../base-pruebas/11-async-await';
+
+
+describe('Prueba 11-async-await', () => { 
+
+    test('getImagen obtener la URL de giphy', async () => { 
+
+        const url = await getImagen();
+        console.log(url);
+        expect(typeof url).toBe('string')
+     })
+ })
