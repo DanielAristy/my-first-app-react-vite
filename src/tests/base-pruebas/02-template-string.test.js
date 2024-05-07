3538c8793e905ac4a07912ecd758842ca7c32957
daniel.aristizabal <daniel.aristizabal@sofka.co.co>
2024-05-07 16:17:48 -0500

    Se realiza prueba de 02-template-string de la funcion getSaludo. Se realiza configuracion de Babel

---
+++ src/tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,13 @@
+import { getSaludo } from '../../base-pruebas/02-template-string';
+
+describe('Pruebas en 02-template-string.js', () => { 
+
+    test('getSaludo de retorna "Hola Daniel"', () => { 
+        const name = 'Daniel';
+
+        const message = getSaludo(name);
+
+        expect( message ).toBe(`Hola ${name}`)
+
+     })
+ })
